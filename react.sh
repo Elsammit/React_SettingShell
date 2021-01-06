af875cf0a7eda8c524c68a91b5ab8fcd1168a40e
Elsammit <hhashimo25@gmail.com>
2021-01-06 23:03:21 +0900

    add comments

--- react.sh
+++ react.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+#make React project.
 create-react-app $1
 
+#add new App.js
 rm $1/src/App.js
 cp App.js ./$1/src/
 
 base=$((6+$#))
+
+#make page file(add & make .js and .css) 
 for x in "$@"
 do
     if [ $x = $1 ]; then
@@ -14,6 +18,7 @@ do
         cp page1.js ./$1/src/$x.js
         touch ./$1/src/$x.css
 
+        #delete and insert sentence in App.js. 
         sed -i -e "2d" ./$1/src/$x.js
         sed -i -e "1a import './"$x".css'" ./$1/src/$x.js
         sed -i -e "4d" ./$1/src/$x.js
@@ -28,10 +33,12 @@ do
     if [ $x = $1 ]; then
         echo "same var"
     else
+        #delete and insert sentence in App.js. 
         sed -i -e $base"a           <Route path='/"$x"' component={"$x"}/>" ./$1/src/App.js
     fi
 done
 
+#install react-router-dom
 yarn add install react-router-dom
 
 cd $1
